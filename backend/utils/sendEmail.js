d274538b65f3e2bc48f4c1bb77ab3eca3d4a0d35
Saqif <saqifhaque@gmail.com>
2023-01-15 00:56:48 +0600

    Nodemailer configured

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,36 @@
+const { default: test } = require('node:test');
+const nodemailer = require('nodemailer');
+
+const sendEmail= async (subject, message, send_to, sent_from, reply_to) => {
+
+    let testAccount = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer;.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: 587,
+        auth: {
+            user: testAccount.user,
+            pass: testAccount.pass,
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    })
+
+    const options = {
+        from: sent_from,
+        to: send_to,
+        replyTo: reply_to,
+        subject: subject,
+        html: message,
+    }
+
+    transporter.sendEmail(options, function(error, info){
+        if(error) {
+            console.log(error);
+        }
+        console.log(info);
+    })
+}
+
+module.exports = sendEmail;
