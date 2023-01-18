2121a14bb9a70154d3b1f898e221744c45484b39
Saqif <saqifhaque@gmail.com>
2023-01-19 01:45:07 +0600

    Create product api added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,29 @@
 const asyncHandler = require('express-async-handler');
+const Product = require("../schema/productSchema");
 
 const createProduct = asyncHandler (async (req, res) => {
-    res.send("Product created");
+    const { name, sku, category, quantity, price, description  } = req.body;
+
+    // Validation
+    if(!name || !category || !quantity || !price || !description ) {
+        res.status(400);
+        throw new Error("Please fill in all fields");
+    }
+
+    // Image Upload
+
+    // Create product
+    const product = await Product.create({
+        user: req.user.id,
+        name,
+        sku,
+        category,
+        quantity,
+        price,
+        description
+    })
+
+    res.status(201).json(product);
 })
 
 module.exports = {
