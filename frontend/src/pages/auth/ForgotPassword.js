d4a4818a9558c4f4ded8046ce5d3d3e03e660b92
Saqif <saqifhaque@gmail.com>
2023-01-24 20:36:27 +0600

    Forgot password component design added

--- frontend/src/pages/auth/ForgotPassword.js
+++ frontend/src/pages/auth/ForgotPassword.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import loginImg from '../../assets/Stock.png';
 
 const ForgotPassoword = () => {
     return (
-        <div>Forgot passoword</div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+            <div className='hidden sm:block'>
+                <img className='w-full h-full object-cover' src={loginImg} alt=''/>
+            </div>
+            <div className='bg-gray-800 flex flex-col justify-center'>
+                <form className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
+                    <h2 className='text-4xl dark:text-white font-bold text-center'>Verification</h2>
+                    <div className='flex flex-col text-gray-400 py-5'>
+                        <label>Email</label>
+                        <input className='form-control' type="text"/>
+                    </div>
+                    <button className='btn-primary'>Verify</button>
+                </form>
+            </div>   
+        </div>
     )
 }
 
