9b82cd8a2c4b6333f820bda622457652c90a3236
Saqif <saqifhaque@gmail.com>
2023-01-25 23:33:55 +0600

    Working on authorization functionality in redux

---
+++ frontend/src/component/layout/layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../header/header';
+import Footer from "../footer/footer";
+
+const layout = () => {
+  return (
+    <div>
+        <Header></Header>
+        <div>
+            body
+        </div>
+        <Footer></Footer>
+    </div>
+  )
+}
+
+export default layout
