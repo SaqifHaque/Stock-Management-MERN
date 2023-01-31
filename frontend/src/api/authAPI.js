673ad38fbfc119a92a423b3ac427e78c9e65e85a
Saqif <saqifhaque@gmail.com>
2023-01-31 21:20:17 +0600

    Product api added

--- frontend/src/api/authAPI.js
+++ frontend/src/api/authAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from 'react-toastify';
 
-export const BACKEND_URL= process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL= process.env.REACT_APP_BACKEND_URL;
 
 export const registerUser = async (userData) => {
     try {
---
+++ frontend/src/api/productAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { toast } from 'react-toastify';
+
+const BACKEND_URL= process.env.REACT_APP_BACKEND_URL;
+const API_URL = `${BACKEND_URL}/api/products`;
+
+
+const createProduct = async (formData) => {
+    const response = await axios.post(API_URL, formData);
+    return response.data;
+}
+
+const productAPI = {
+    createProduct
+}
+
+export default productAPI;
