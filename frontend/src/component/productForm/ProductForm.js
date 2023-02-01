5f09f3b672e70fc6a4998406f674a695ded62c57
Saqif <saqifhaque@gmail.com>
2023-02-01 19:15:42 +0600

    Quill package added and product form configuration

--- frontend/src/component/productForm/ProductForm.js
+++ frontend/src/component/productForm/ProductForm.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const ProductForm = ({product, productImage, imagePreview, description, setDescription, handleInputChange,
 handleImageChange, saveProduct}) => {
   return (
-    <form className='max-w-[1000px] w-full mx-auto bg-gray-900 p-8 mt-5 rounded-lg'>
+    <form className='max-w-[1000px] w-full mx-auto bg-gray-900 p-8 m-5 rounded-lg'>
         <h2 className='text-4xl dark:text-white font-bold text-center'>Add Product</h2>
+        <div class="form-group">
+        <label>Product Image</label>
+            <label
+                draggable
+                onDragStart={(e) => handleImageChange(e)}
+           onDrag={(e) => handleImageChange(e)}
+           onDragEnd={(e) => handleImageChange(e)}
+                class="form-file-label">
+                <span class="flex items-center space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                    </svg>
+                    <span class="font-medium text-gray-400">
+                        Drop files to Attach, or
+                        <span class="text-blue-600 underline">browse</span>
+                    </span>
+                </span>
+                <input type="file" name="photo" class="hidden" onChange={(e) => handleImageChange(e)}/>
+            </label>
+            {imagePreview != null ? (
+                <div>
+                    <img className="max-wdith: 400px max-w-screen-sm mt-5" src={imagePreview} alt="Product"/>
+                </div>
+            ) : (<p>No Image set for this product</p>)}
+        </div>
         <div className='form-group'>
             <label>Product Name</label>
             <input className='form-control' type="text" name="name" onChange={handleInputChange} value={product?.name} required/>
@@ -21,26 +50,50 @@ handleImageChange, saveProduct}) => {
             <label>Product Quantity</label>
             <input className='form-control' type="text" name="quantity" onChange={handleInputChange} value={product?.quantity} required/>
         </div>
-        <div class="form-group">
-            <label
-                class="form-file-label">
-                <span class="flex items-center space-x-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-                    </svg>
-                    <span class="font-medium text-gray-400">
-                        Drop files to Attach, or
-                        <span class="text-blue-600 underline">browse</span>
-                    </span>
-                </span>
-                <input type="file" name="photo" class="hidden" onChange={handleInputChange}/>
-            </label>
+        <div className='form-group space-y-3'>
+            <label>Product Description</label>
+            <ReactQuill theme="snow" value={description} onChange={setDescription} modules={ProductForm.modules} formats={ProductForm.formats}/>
         </div>
-        <button className='btn-primary'>Add Product</button>
+        <button onClick={saveProduct} className='btn-primary'>Add Product</button>
     </form>
   )
 }
+
+ProductForm.modules = {
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ align: [] }],
+      [{ color: [] }, { background: [] }],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["clean"],
+    ],
+  };
+  ProductForm.formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "color",
+    "background",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "video",
+    "image",
+    "code-block",
+    "align",
+  ];
 
 export default ProductForm
