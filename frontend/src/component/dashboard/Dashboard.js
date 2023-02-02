bc7446bb03f7ee049de43ff74ff51cd0694e6af5
Saqif <saqifhaque@gmail.com>
2023-02-02 19:50:48 +0600

    Working on dashboard product list

--- frontend/src/component/dashboard/Dashboard.js
+++ frontend/src/component/dashboard/Dashboard.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import authorization from "../../customHook/authorization";
+import { selectIsLoggedIn } from '../../redux/features/auth/authSlice';
+import { getProducts } from '../../redux/features/product/productSlice';
+import ProductList from '../product/ProductList';
+import {VscEye}from "react-icons/vsc";
 
 const Dashboard = () => {
+  // authorization("/login");
+  const dispatch = useDispatch;
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const {products, isLoading, isError, message} = useSelector((state) => state.product);
+
+  useEffect(() => {
+    if(isLoggedIn) {
+      dispatch(getProducts());
+    }
+
+    if(isError){
+      console.log(message);
+    }
+  }, [isLoggedIn,isError, message, dispatch])
+  
+
   return (
-    <div>dashboard</div>
+    <div className='h-full w-full bg-gray-800 flex flex-col'>
+        <h1 className="text-gray-400">Dashboard</h1>
+        <ProductList products={products} isLoading={isLoading}/>
+    </div>
   )
 }
 
