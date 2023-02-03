e85ca22f123b7306195c21c486ce08daf9adbcf1
Saqif <saqifhaque@gmail.com>
2023-02-03 17:12:14 +0600

    Patch delete api added for product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -28,7 +28,7 @@ const createProduct = asyncHandler (async (req, res) => {
             fileName: req.file.originalname,
             filePath: uploadedFile.secure_url,
             fileType: req.file.mimetype,
-            fileSize: fileSizeFormatter(req.file.size)
+            fileSize: fileSizeFormatter(req.file.size, 2)
         }
     }
     // Create product
@@ -67,9 +67,82 @@ const getProduct = asyncHandler(async (req, res) => {
     res.status(200).json(product);
 })
 
+// Delete Product
+const deleteProduct = asyncHandler (async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if(!product) {
+        res.status(404);
+        throw new Error("Product not found");
+    }
+
+    if(product.user.toString() != req.user.id){
+        res.status(401)
+        throw new Error("User not Authorized")
+    }
+    await product.remove();
+
+    res.status(200).json({message: "Product Deleted."});
+})
+
+// Update Product
+const updateProduct = asyncHandler(async (req, res) => {
+    const { name, category, quantity, price, description  } = req.body;
+    const {id} = req.params;
+
+    const product = await Product.findById(id);
+
+    if(!product) {
+        res.status(404);
+        throw new Error("Product not found");
+    }
+
+    if(product.user.toString() != req.user.id){
+        res.status(401)
+        throw new Error("User not Authorized")
+    }
+
+    let fileData = {};
+    if(req.file) {
+        // save Image to cloudinary
+        let uploadFile;
+        try{
+            uploadedFile = await cloudinary.uploader.upload(req.file.path, {folder: "StockM", resource_type: "image"})
+        } catch {
+            res.status(500);
+            throw new Error("Image could not be uploaded")
+        }
+
+        fileData = {
+            fileName: req.file.originalname,
+            filePath: uploadedFile.secure_url,
+            fileType: req.file.mimetype,
+            fileSize: fileSizeFormatter(req.file.size, 2)
+        }
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+        {_id: id},
+        {
+            name,
+            category,
+            quantity,
+            price,
+            description,
+            image: Object.keys(fileData).length === 0 ? product?.image : fileData
+        },
+        {
+            new: true,
+            runValidators:true
+        }
+    )
+    res.status(200).json(updatedProduct)
+}) 
+
 
 module.exports = {
     createProduct,
     getProducts,
-    getProduct
+    getProduct,
+    deleteProduct,
+    updateProduct
 }
